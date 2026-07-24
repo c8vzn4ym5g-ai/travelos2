f9fd183b3c51eef45dd88a495d3080205d9056f3
c8vzn4ym5g-ai <c8vzn4ym5g@privaterelay.appleid.com>
2026-07-25 00:39:45 +0800

    Render Travelpayouts Drive as native script

--- components/travelpayouts-drive.tsx
+++ components/travelpayouts-drive.tsx
@@ -1,10 +1,8 @@
-import Script from "next/script";
-
 const driveScriptUrl = "https://emrldtp.cc/NTUwMzEz.js?t=550313";
 
 export function TravelpayoutsDrive() {
   return (
-    <Script
+    <script
       data-cfasync="false"
       data-no-defer="1"
       data-noptimize="1"
@@ -12,7 +10,6 @@ export function TravelpayoutsDrive() {
       id="travelpayouts-drive"
       seraph-accel-crit="1"
       src={driveScriptUrl}
-      strategy="afterInteractive"
     />
   );
 }
